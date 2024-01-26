12c878116c0e77074643ac7a0b8c35bf5e5cf87b
jeferson <jeh.uninove@gmail.com>
2024-01-25 23:05:19 -0300

    remoção do drop

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,3 @@
-drop table video_categoria;
-
 CREATE TABLE IF NOT EXISTS video_categoria (id INT NOT NULL AUTO_INCREMENT, titulo VARCHAR(255),  PRIMARY KEY (id));
 
 CREATE TABLE IF NOT EXISTS tutorial (id INT NOT NULL AUTO_INCREMENT, title VARCHAR(255), description VARCHAR(255), published BOOLEAN, PRIMARY KEY (id));
