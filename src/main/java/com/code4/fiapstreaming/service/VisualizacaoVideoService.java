b1b6d481bb8ea275969026783a8047095e802c0a
jeh.uninove@gmail.com <jeh.uninove@gmail.com>
2024-01-29 22:36:02 -0300

    Item 8

--- src/main/java/com/code4/fiapstreaming/service/VisualizacaoVideoService.java
+++ src/main/java/com/code4/fiapstreaming/service/VisualizacaoVideoService.java
@@ -13,46 +13,46 @@ import java.util.UUID;
 
 @Service
 public class VisualizacaoVideoService {
-    @Autowired
-    VisualizacaoVideoRepository visualizacaoVideoRepository;
-    public Flux<VisualizacaoVideo> findAll() {
-        return visualizacaoVideoRepository.findAll();
-    }
-    public Mono<VisualizacaoVideo> findById(UUID id){
-        return visualizacaoVideoRepository.findById(id);
-    }
-    public Mono<VisualizacaoVideo> save(VisualizacaoVideo visualizacaoVideo) {
-        return visualizacaoVideoRepository.save(visualizacaoVideo);
-    }
-    public Mono<VisualizacaoVideo> update(UUID id, VisualizacaoVideo visualizacaoVideo) {
-        return visualizacaoVideoRepository.findById(id).map(Optional::of).defaultIfEmpty(Optional.empty())
-                .flatMap(optionalVideoCategoria -> {
-                    if (optionalVideoCategoria.isPresent()) {
-                        visualizacaoVideo.setId(id);
-                        return visualizacaoVideoRepository.save(visualizacaoVideo);
-                    }
+	@Autowired
+	VisualizacaoVideoRepository visualizacaoVideoRepository;
 
-                    return Mono.empty();
-                });
-    }
-    public Mono<Void> deleteById(UUID id) {
-        return visualizacaoVideoRepository.deleteById(id);
-    }
-    
-    public Mono<Double> calculateMediaVisualizacoes() {
-        return visualizacaoVideoRepository.findAll()
-                                          .map(VisualizacaoVideo::getId)
-                                          .collectList()
-                                          .map(ids -> {
-                                              double totalVisualizacoes = ids.size();
-                                              if (totalVisualizacoes == 0) {
-                                                  return 0.0;
-                                              } else {
-                                                 
-                                                  return totalVisualizacoes / totalVisualizacoes;
-                                              }
-                                          });
-    }
+	public Flux<VisualizacaoVideo> findAll() {
+		return visualizacaoVideoRepository.findAll();
+	}
+
+	public Mono<VisualizacaoVideo> findById(UUID id) {
+		return visualizacaoVideoRepository.findById(id);
+	}
+
+	public Mono<VisualizacaoVideo> save(VisualizacaoVideo visualizacaoVideo) {
+		return visualizacaoVideoRepository.save(visualizacaoVideo);
+	}
+
+	public Mono<VisualizacaoVideo> update(UUID id, VisualizacaoVideo visualizacaoVideo) {
+		return visualizacaoVideoRepository.findById(id).map(Optional::of).defaultIfEmpty(Optional.empty())
+				.flatMap(optionalVideoCategoria -> {
+					if (optionalVideoCategoria.isPresent()) {
+						visualizacaoVideo.setId(id);
+						return visualizacaoVideoRepository.save(visualizacaoVideo);
+					}
+
+					return Mono.empty();
+				});
+	}
+
+	public Mono<Void> deleteById(UUID id) {
+		return visualizacaoVideoRepository.deleteById(id);
+	}
+
+	public Mono<Double> calculateMediaVisualizacoes() {
+		return visualizacaoVideoRepository.findAll().map(VisualizacaoVideo::getId).collectList().map(ids -> {
+			double totalVisualizacoes = ids.size();
+			if (totalVisualizacoes == 0) {
+				return 0.0;
+			} else {
+				//contador de media de Visualizações
+				return totalVisualizacoes / totalVisualizacoes;
+			}
+		});
+	}
 }
-
-
